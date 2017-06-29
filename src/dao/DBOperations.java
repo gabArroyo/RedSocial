d32cffc91217eaaf05fdcc6562cf7d33825e42fd
Brayan <cbr4yan@gmail.com>
2017-06-29 21:41:18 +0200

    DAO mod

--- src/dao/DBOperations.java
+++ src/dao/DBOperations.java
@@ -4,12 +4,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBOperations {
-	public static int getSizeResultSet(ResultSet resultSet) throws SQLException{
-		int size = 0;
-		if (resultSet.last()) {
-			size = resultSet.getRow();
-			resultSet.beforeFirst();
-		}
-		return size;
-	}
+  public static int getSizeResultSet(ResultSet resultSet) throws SQLException {
+    int size = 0;
+    if (resultSet != null) {
+      if (resultSet.last()) {
+        size = resultSet.getRow();
+        resultSet.beforeFirst();
+      }
+      return size;
+    }
+    return 0;
+  }
 }
